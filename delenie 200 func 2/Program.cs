7e0f5aceb1d0bed9227c4a5eca49753e2f6f07ba
Vcyprus32@mail.ru <Vcyprus32@mail.ru>
2023-01-15 10:57:07 +0200

    Добавьте файлы проекта.

---
+++ delenie 200 func 2/Program.cs
@@ -0,0 +1,104 @@
+﻿// напечатать мин число >200 кратное 17 
+
+//int a;
+//int b;
+/*
+int A()
+{ 
+Console.WriteLine($"Введите минимальное число от 200, которое кратно 17");
+int a = int.Parse(Console.ReadLine( ));
+    return a;
+}
+int C = A();
+void B(int C)
+{ 
+while (C < 200)
+{
+    Console.WriteLine($"Введите минимальное число ОТ 200, кратное 17");
+    C = int.Parse(Console.ReadLine());
+} 
+while (C > 199)
+{
+    
+    Console.WriteLine($"Это число не кратно 17, попробуйте снвоа");
+    C = int.Parse(Console.ReadLine());
+    int b = C / 17;
+    if (b == 12)
+    {
+        Console.WriteLine($" Минимальное число от 200 кратное 17 = {C} ");
+    }
+
+}
+}
+*/
+/*
+int a;
+int b;
+int A()
+{
+    Console.WriteLine($"Введите минимальное число от 200, которое кратно 17");
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+int C = A();
+
+static int B(int C)
+{
+    while (C < 200)
+    {
+        Console.WriteLine($"Введите минимальное число ОТ 200, кратное 17");
+        C = int.Parse(Console.ReadLine());
+    }
+    while (C > 199)
+    {
+        int b = C / 17;
+        if (b == 12)
+        {
+            Console.WriteLine($" Минимальное число от 200 кратное 17 = {C} ");
+        }
+        break;
+        Console.WriteLine($"Это число не кратно 17, попробуйте снвоа");
+        C = int.Parse(Console.ReadLine());
+    }
+    return C;
+}
+Console.WriteLine(B(C));
+*/
+
+int a;
+int b;
+int A()
+{
+    Console.WriteLine($"Введите минимальное число от 200, которое кратно 17");
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+int C = A();
+
+static int B(int C)
+{
+    bool c = true;
+    while (C < 200)
+    {
+        Console.WriteLine($"Введите минимальное число ОТ 200, кратное 17");
+        C = int.Parse(Console.ReadLine());
+    }
+    while (c)
+    {
+
+        double b = C / 17;
+        if (b == 12)
+        {
+            Console.WriteLine($" Минимальное число от 200 кратное 17 = {C} ");
+            c = false;
+        }
+        else
+        {
+            Console.WriteLine($"Это число не кратно 17, попробуйте снвоа");
+            C = int.Parse(Console.ReadLine());
+        }
+
+    }
+    return C;
+}
+Console.WriteLine(B(C));
